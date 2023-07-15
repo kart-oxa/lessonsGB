132b1e1620342e69c18819fc9b84d35499aebc2f
Anna <annfateeva02@mail.ru>
2023-07-15 19:15:13 +0300

    Added Task24...Task30

---
+++ Task25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: (branch task_1)
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое положительное число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB <= 0)
+{
+Console.WriteLine("Некорректный ввод!");
+return;
+}
+
+int degreeNumbers = DegreeNumbers(numberA, numberB);
+Console.WriteLine($"Число {numberA} в натуральной степени числа {numberB} = {degreeNumbers}");
+
+int DegreeNumbers(int a, int b)
+{
+    int deg = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        deg = deg * a;
+    }
+    return deg;
+}
---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNumber(number);
+
+Console.WriteLine($"{number} -> {result}");
+
+int SumNumber(int num)
+{
+int count = 0;
+int sum = 0;
+while (num != 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+    count++;
+}
+return count == 0 ? 1 : sum;
+}
