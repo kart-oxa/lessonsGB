12fa1b629ec8d8ff0e0b7e15f1bc87e74408e95e
Anna <annfateeva02@mail.ru>
2023-07-18 00:21:27 +0300

    Added Task39...Task45

---
+++ Task42/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 46 -> 101110
+// 13 -> 1101
+// 2 -> 10
+
+Console.WriteLine("Введите положительное целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(DecToBinStr(number));
+
+int DecToBin(int numb)
+{
+int result = 0;
+int factor = 1;
+while (numb != 0)
+{
+result = result + numb % 2 * factor;
+numb /= 2;
+factor *= 10;
+}
+return result;
+
+}
+
+string DecToBinStr(int numb)
+{
+string result = string.Empty;
+while (numb != 0)
+{
+result = numb % 2 + result;
+numb /= 2;
+
+}
+return result; // 13 -> 1, 6 -> 0, 3 -> 1, 1 -> 1, 0
+
+}
---
+++ Task45/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+int[] array = CreateArrayRndInt(5, 100, 1000);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
+
+int[] newArray = CopyArray(array);
+Console.Write("[");
+PrintArray(newArray);
+Console.Write("]");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(min, max + 1);
+}
+return arr;
+}
+
+void PrintArray(int[] arr, string sep = ", ")
+{
+for (int i = 0; i < arr.Length; i++)
+{
+if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep}");
+else Console.Write($"{arr[i]}");
+}
+}
+
+int[] CopyArray(int[] arr)
+{
+int[] newArr = new int[arr.Length];
+for (int i = 0; i < arr.Length; i++)
+{
+newArr[i] = arr[i];
+}
+return newArr;
+}
