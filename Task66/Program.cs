75ae951e5378b2534e9c6e576c3b220ab21ddca6
Anna <annfateeva02@mail.ru>
2023-07-30 14:24:42 +0300

    Added Task63...Task69

---
+++ Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое натуральное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int temp = 0;
+if (number1 > number2) 
+{
+    temp = number1;
+    number1 = number2;
+    number2 = temp;
+}
+int SumNaturalDigits(int num1, int num2)
+{
+        if (num2 == num1) return num1;
+        return num1 + SumNaturalDigits(num1 + 1, num2);
+}
+
+Console.WriteLine(SumNaturalDigits(number1, number2));
---
+++ Task67/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumDigits(int num)
+{
+if (num == 0) return 0;
+return num % 10 + SumDigits(num / 10); //num = 453 / 10 -> 45 / 10 -> 4 /10 -> 0
+}
+
+Console.WriteLine(SumDigits(45));
+
+// int FactorialRec(int n)
+// {
+// if (n == 1) return 1;
+// else return n * FactorialRec(n - 1);
+// }
+
+// Console.WriteLine(FactorialRec(5));
